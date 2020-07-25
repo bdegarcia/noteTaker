5456246731ae0b5bd97df7ded9061daacefaf8b6
Ben de Garcia <degarcia.ben@gmail.com>
2020-07-25 16:28:36 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+//this starts the server
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+  console.log('functional')
 });
-
+//routes the html to the index page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
+//connects the notes.html
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -24,16 +24,16 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function (req, res) {
   res.json(notes);
 });
-
+//adding info to the db file
 app.post("/api/notes", function (req, res) {
   const newNote = req.body;
-  console.log(newNote);
+  //adding an id for deleting purposes later
   newNote.id = parseInt(notes.length) + 1;
   fs.readFile(noteFile, "utf-8", (err, data) => {
     let currentNotes = JSON.parse(data);
+    //here the inputted info is added to the array in the db file
     currentNotes.push(newNote);
-    fs.writeFile(noteFile, JSON.stringify(currentNotes), (err) => {
-      if (err) throw err;
+    fs.writeFile(noteFile, JSON.stringify(currentNotes), (err) => { if (err) throw err;
       res.json(currentNotes);
     });
   });
@@ -41,20 +41,12 @@ app.post("/api/notes", function (req, res) {
 
 app.delete("/api/notes/:id", function (req, res) {
   const deleteId = req.params.id;
-    notes = notes.filter((note) =>{
-      return note.id != deleteId})
-    fs.writeFile(noteFile, JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json(notes);
-    });
+  //here I use filter to remove the one specific object in the notes array
+  notes = notes.filter((note) => {
+    return note.id != deleteId;
   });
-
-
-function readDatabase(something) {
-  fs.readFile(noteFile, "utf-8", (err, data) => {
-    let newInfo = JSON.parse(data);
-    console.log(newInfo);
+  fs.writeFile(noteFile, JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    res.json(notes);
   });
-}
-
-readDatabase();
+});
