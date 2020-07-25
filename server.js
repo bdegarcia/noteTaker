06a01b17737354307f517c259a5531374266412f
Ben de Garcia <degarcia.ben@gmail.com>
2020-07-24 21:32:51 -0500

    added post function

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const express = require('express');
+const fs= require('fs');
 const app = express();
+const noteFile = './public/assets/db.json'
+const notes = require(noteFile)
 const PORT = process.env.PORT || 8000;
-
-const notes = [];
-
-
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -22,3 +19,21 @@ app.get("/", function(req, res) {
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.get("/api/notes", function(req, res) {
+  res.json(notes);
+});
+
+app.post("/api/notes", function(req, res) {
+  const newNote = req.body;
+  newNote.id = parseInt(notes.length) + 1
+  fs.appendFile(noteFile, newNote, () => {
+    if (err) throw err;
+    res.json(notes)
+  });
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+  const deleteId = req.params.id
+  
+})
